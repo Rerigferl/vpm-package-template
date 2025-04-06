8a2fae3e9d94e3d393bf304a14fa53283230542a
Rerigferl <70315656+AshleyScarlet@users.noreply.github.com>
2025-04-06 16:27:12 +0900

    add global usings

---
+++ Common~/GlobalUsings.cs
@@ -0,0 +1,13 @@
+global using System;
+global using System.Collections;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Runtime.CompilerServices;
+global using UnityEngine;
+
+#if UNITY_EDITOR
+global using UnityEditor;
+global using UnityEditor.Animations;
+#endif
+
+global using Object = UnityEngine.Object;
